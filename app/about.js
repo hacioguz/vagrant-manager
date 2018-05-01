4417816819d302f301a1ec744ddb6b21d8f27061
Lawrence Meckan <media@absalom.biz>
2018-05-01 23:24:45 +1000

    Open for URLs

--- app/about.js
+++ app/about.js
@@ -1,5 +1,6 @@
-const {app,shell, ipcRenderer} = require('electron')
+const {app, shell, ipcRenderer} = require('electron')
 const remote = require('electron').remote
+const open = require('open')
 const VersionChecker = require('./utils/versionChecker')
 const HtmlTranslate = require('./utils/htmlTranslate')
 
@@ -11,6 +12,11 @@ document.getElementById('versioner').innerHTML('v'+app.getVersion())
 
 document.addEventListener('dragover', event => event.preventDefault())
 document.addEventListener('drop', event => event.preventDefault())
+
+document.on('new-window', function(event, url){
+  event.preventDefault()
+  open(url)
+})
 
 document.getElementById('close-btn').addEventListener('click', function (e) {
   window.close()
